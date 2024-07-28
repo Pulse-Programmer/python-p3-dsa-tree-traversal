bbace01cc6be4f0f281f702b79f562bf91e971c8
Stephen Ochieng <materiamystic@gmail.com>
2024-07-28 03:46:27 +0300

    adds DFS and BFS tree algorithms

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,22 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+    
+    # Depth-first search (DFS) to find the node with the given ID in the tree.
+    while nodes_to_visit:
+      node = nodes_to_visit.pop(0)
+      if id == node.get('id'):
+        return node
+      nodes_to_visit = node['children'] + nodes_to_visit
+      
+    return None
+
+    # Breadth-first search (BFS) to find the node with the given ID in the tree.
+    while nodes_to_visit:
+      node = nodes_to_visit.pop(0)
+      if id == node.get('id'):
+        return node
+      nodes_to_visit += node['children']
+      
+    return None
